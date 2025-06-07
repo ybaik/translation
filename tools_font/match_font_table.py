7e81b38b15030b920c9d15f30fa20f600dd401ca
Youngki Baik <hyunxx@gmail.com>
2025-06-07 13:13:46 +0900

    Font table updated

--- tools_font/match_font_table.py
+++ tools_font/match_font_table.py
@@ -5,8 +5,11 @@ from pathlib import Path
 def main():
     base_path = Path("./font_table/")
 
-    src_path = base_path / "font_table-jpn-60k.json"
+    src_path = Path("./data") / "shiftjis.json"
+
+    src_path = base_path / "font_table-jpn-60K.json"
     dst_path = base_path / "font_table-jpn-full.json"
+
 
     with open(src_path, "r", encoding="utf-8") as f:
         src_font_table = json.load(f)
@@ -17,11 +20,17 @@ def main():
 
     for key, src_value in src_font_table.items():
         src_code = int(key, 16)
-        if src_code < int("889F", 16):
-            continue
-        # if src_code > int ("9872", 16):
+        # if src_code < int("889F", 16):
+        #     continue
+        # if src_code < int ("9FFC", 16):
         #     continue
 
+        # if src_code > int ("9872", 16):
+        #     continue
+        # if src_code > int ("9FFC", 16):
+        #     continue
+        if src_code > int ("EAA4", 16):
+            continue
 
         dst_value = dst_font_table[key]
 
